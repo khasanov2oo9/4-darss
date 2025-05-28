1a681a3c02923265114b678a101d3d4063c487b8
khasanov2oo9 <fazliddinhasanov34@gmail.com>
2025-05-28 18:12:47 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let name = prompt("Ismingizni kiriting")
+
+let surname = prompt("Birorta harf kiriting")
+
+if (surname) {
+    alert(`${name} ismida ${surname} harfi mavjud.`)
+} else { 
+    alert(`${names} ismida ${surnames} harfi mavjud emas.`)
+};
+
+let names = prompt("Ismingizni kiriting");
+
+let surnames = prompt("Birorta harf kiriting");
+
+if (surnames){
+    alert(`${name} ismida ${surname} harfi mavjud.`)
+} else {
+    alert(`${names}  ismida ${surnames} harfi mavjud emas.`)
+};
